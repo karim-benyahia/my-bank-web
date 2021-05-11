bcffb446c755136f12ddb519532fc6a75a78d5bd
karim <karim.benyahia@serli.com>
2021-05-11 16:33:36 +0200

    first commit

---
+++ src/main/java/fr/kb/mybankweb/MyBankWebApplication.java
@@ -0,0 +1,13 @@
+package fr.kb.mybankweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBankWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/kb/mybankweb/MyBankWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kb.mybankweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
